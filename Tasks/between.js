6fee4c6db6df4513b06fe645d5cfa014890ba703
ferer00 <155906971+ferer00@users.noreply.github.com>
2024-01-14 23:02:21 +0200

    Update between.js
    Змінено ім'я параметра функції

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,10 +1,10 @@
 // Extract substring between prefix and suffix
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p);
+getvaluebetween = (str, startIndex, s) => {
+  i = str.indexOf(startIndex);
   if (i === -1) return '';
   else {
-    k = i + p.length;
+    k = i + startIndex.length;
     str = str.substring(k);
     if (s) {
       i = str.indexOf(s);
