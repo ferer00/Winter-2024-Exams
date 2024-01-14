7155b0fb9e19ee19c3b795e7839a62ece86d29f6
ferer00 <155906971+ferer00@users.noreply.github.com>
2024-01-14 23:58:43 +0200

    Update day.js
    створив змінну у самому циклі

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,8 +3,8 @@
 const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (input) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
+  
+  for (let i = 0; i < D.length; i++) {
     if (input.startsWith(D[i].toLowerCase())) {
       return i + 1;
     }
